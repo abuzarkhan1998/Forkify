a2d5d3a6b90394192b26ea0ff571e1dfbb7ad3c2
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2025-08-29 16:17:35 +0530

    Loading the recipe and logging in console

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,28 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function(){
+try{
+  // const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+  const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+  const data = await res.json();
+  let {recipe} = data?.data;
+  recipe = {
+    id: recipe.id,
+    ingredients: recipe.ingredients,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    ingredients: recipe.ingredients,
+    image: recipe.image_url,
+    source: recipe.source_url
+  };
+  console.log(recipe);
+  console.log('Hello');
+}
+catch(err){
+  alert(err);
+}
+};
+
+showRecipe();
